c57481dd977da1dec4e357fb4a2bb57ee5535839
Marc <marcus@rams.services>
2023-05-30 09:54:50 +0300

    added intf and impl

---
+++ src/rams/domain/auth/impl/core.clj
@@ -0,0 +1,4 @@
+(ns rams.domain.auth.impl.core)
+
+(defn main []
+  (println "Hello world"))
---
+++ src/rams/domain/auth/intf/core.clj
@@ -0,0 +1,5 @@
+(ns rams.domain.auth.intf.core
+  (:require [rams.domain.auth.impl.core :as impl]))
+
+(defn main []
+  (impl/main))
